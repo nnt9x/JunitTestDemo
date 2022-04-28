a5e5f475d47b511ca6b36b3e4cd3caad031906ab
NNT9x <42613109+nnt9x@users.noreply.github.com>
2022-04-28 21:39:00 +0700

    Initial commit

---
+++ src/main/java/MayTinh.java
@@ -0,0 +1,24 @@
+import org.decimal4j.util.DoubleRounder;
+
+public class MayTinh {
+    public long tong(long a, long b) {
+        return a + b;
+    }
+
+    public long hieu(long a, long b) {
+        return a - b;
+    }
+
+    public long tich(long a, long b) {
+        return a * b;
+    }
+
+    public double thuong(long a, long b) {
+        if (b == 0) {
+            throw new RuntimeException("Không thực hiện được phép chia!");
+        }
+        double tmp = (double) a / b;
+        return DoubleRounder.round(tmp, 3);
+    }
+
+}
---
+++ src/test/java/MayTinhTest.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+class MayTinhTest {
+
+    private final MayTinh mayTinh = new MayTinh();
+
+    @Test
+    @DisplayName("Tổng của 5 và 6 nên bằng 11")
+    void tong5Va6Bang11() {
+        long expected = 11;
+        long actual = mayTinh.tong(5, 6);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Hiệu của 5 và 6 là 11")
+    void hieu() {
+        long expected = -1;
+        long actual = mayTinh.hieu(5, 6);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void tich() {
+        long expected = 30;
+        long actual = mayTinh.tich(5, 6);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Thương của 5 và 2 nên bằng 2.5")
+    void thuong() {
+        double expected = 2.5;
+        double actual = mayTinh.thuong(5, 2);
+        Assertions.assertEquals(expected, actual, 0.0001);
+    }
+
+    @Test
+    @DisplayName("Thương của 5 và 0 tạo ra Exception")
+    void thuongException() {
+        RuntimeException exception = Assertions.assertThrows(
+                RuntimeException.class, new Executable() {
+                    @Override
+                    public void execute() throws Throwable {
+                        mayTinh.thuong(5, 0);
+                    }
+                }
+        );
+        Assertions.assertEquals("Không thực hiện được phép chia!",
+                exception.getMessage());
+
+    }
+
+    @Test
+    @DisplayName("Thương của 3 và 11 nên là 0.273")
+    void thuong3Va11() {
+        double expected = 0.273;
+        double actual = mayTinh.thuong(3, 11);
+        Assertions.assertEquals(expected, actual, 0.0001);
+    }
+}
